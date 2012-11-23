9ed4a7c255c7e83df1fa55069c108b040475f8e2
anton <akropp@gmail.com>
2012-11-23 17:44:42 -0500

    comments

--- FSharpToys/Program.fs
+++ FSharpToys/Program.fs
@@ -71,6 +71,12 @@ let printBoard board =
 // set position on  board
 //---------------------------------
 
+(*
+Update token helper. Takes the target token to change to, the current token we are on
+(since it's used in a folding curry'd function), and tests if its a valid move or not.
+If it's not valid, it transform the current item to itself, if it is a vlaid move
+it will transform the current item into the target item
+*)
 let updateToken targetItem doUpdate currentItem = 
     if doUpdate() then 
         match currentItem with
@@ -80,6 +86,13 @@ let updateToken targetItem doUpdate currentItem =
 
     else currentItem
 
+(*
+We need to recursively rebuild the board after setting a token. This helper does that
+The iter option  looks for the target row. When it finds ht etarget row it applies a map 
+function to the row to transform its elements. The map function finds the right element and
+transforms it into the new token (if valid) and returns a new row with the elements updated
+Then we can finish rebuilding the remainder of the board
+*)
 let rec setTokenHelper board (row, col) token iter = 
     if iter = row then
         let counter = fun x -> x + 1
@@ -107,6 +120,11 @@ let setToken board (row,col) token = reverseList (setTokenHelper board (row,col)
 // detect game over
 //=======================================
 
+(*
+Function to test if a row has all the same element. CountBy aggregates the list into 
+(item, count) so we can fold the list into a list of tuples that tells us 
+how many of the cell elements existed.  We can test each type of winner
+*)
 let rowWin row = 
     let sequences = row |> Seq.countBy id |> Seq.toList
 
@@ -119,6 +137,11 @@ let rowWin row =
 
     testSequences sequences
    
+(*
+Helper function to test a group of elements. All elements have to be of the same type
+or they don't win.  We can pass in a recursion function so that we basically get the next 
+set of elements to test.  
+*)
 let testWin elements recursion wintype =
     let win = rowWin elements
     match win with 
@@ -128,12 +151,19 @@ let testWin elements recursion wintype =
                                             else recursion()
         | (_) -> recursion() 
 
+(*
+The easiest case with just testing each row
+*)
 let rec gameOverRows board = 
     match board with
         | [] -> false
         | h::t -> testWin h (fun () -> gameOverRows t) "row"
 
-
+(* 
+Check the columns. For each row in the board we want to get the same element
+and create a list out of that. That is the folded column element. We can then treat
+this as a generic "row" of elements and pass it to our test win function
+*)
 let rec gameOverCols board start = 
     if start >= List.length board then 
         false
@@ -142,6 +172,11 @@ let rec gameOverCols board start =
         testWin elements (fun () -> gameOverCols board (start+1)) "column"
     
 
+(*
+We fold the board by selecting each element and updating our accumulator index
+This lets us get item 0, then item 1, then item 2 and leveage getting an element in a row
+By psasing in the "updater" function we can either incremenet or decrement where we start from
+*)
 let gameOverDiags board start updater = 
     let elements = List.fold(fun acc row -> 
                 let index = snd acc
@@ -153,6 +188,9 @@ let gameOverDiags board start updater =
                                               
     testWin item (fun () -> false) "diagonal"
 
+(*
+ Lazily check all the combinatoins: row, columns, diagonals
+*)
 let gameOver board = 
     let rowWin = gameOverRows board
     let colWin = gameOverCols board 0
